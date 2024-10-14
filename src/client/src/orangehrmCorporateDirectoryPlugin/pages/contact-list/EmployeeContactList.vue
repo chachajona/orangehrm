dec575559136e81ec13a3affeb12975d1229278a
Dung Dang <dung.dang@vietunion.com.vn>
2024-10-14 17:13:47 +0700

    update Contact List (frontend)

--- src/client/src/orangehrmCorporateDirectoryPlugin/pages/contact-list/EmployeeContactList.vue
+++ src/client/src/orangehrmCorporateDirectoryPlugin/pages/contact-list/EmployeeContactList.vue
@@ -19,310 +19,288 @@
 
 <template>
   <div class="orangehrm-background-container">
-    <div class="orangehrm-card-container">
-      <oxd-text tag="h6" class="orangehrm-main-title">
-        Employee Contact List
-      </oxd-text>
-      <oxd-divider />
-
-      <oxd-form :loading="isLoading" @submit-valid="onSave">
+    <oxd-table-filter :filter-title="$t('Employee Contact List')">
+      <oxd-form @submit-valid="onSearch" @reset="onReset">
         <oxd-form-row>
-          <oxd-grid :cols="2" class="orangehrm-full-width-grid">
-            <oxd-grid-item>
-              <oxd-input-field
-                v-model="report.name"
-                :label="$t('general.report_name')"
-                :rules="rules.name"
-              />
-            </oxd-grid-item>
-          </oxd-grid>
-        </oxd-form-row>
-
-        <oxd-divider />
-        <oxd-form-row>
-          <oxd-text class="orangehrm-sub-title" tag="h6">
-            {{ $t('pim.selection_criteria') }}
-          </oxd-text>
-          <oxd-grid :cols="4" class="orangehrm-full-width-grid">
-            <oxd-grid-item class="orangehrm-report-criteria --span-column-2">
-              <oxd-input-field
-                v-model="report.criterion"
-                type="select"
-                :label="$t('pim.selection_criteria')"
-                :options="availableCriteria"
-              />
-              <oxd-input-group>
-                <oxd-icon-button
-                  class="orangehrm-report-icon"
-                  name="plus"
-                  @click="addCriterion"
-                />
-              </oxd-input-group>
-            </oxd-grid-item>
-            <oxd-grid-item>
-              <oxd-input-field
-                v-model="report.includeEmployees"
-                type="select"
-                :label="$t('pim.include')"
-                :options="includeOpts"
-                :rules="rules.includeEmployees"
-              />
-            </oxd-grid-item>
-            <contact-criterion
-              v-for="(criterion, index) in report.criteriaSelected"
-              :key="criterion"
-              v-model:operator="
-                report.criteriaFieldValues[criterion.id].operator
-              "
-              v-model:valueX="report.criteriaFieldValues[criterion.id].valueX"
-              v-model:valueY="report.criteriaFieldValues[criterion.id].valueY"
-              :criterion="criterion"
-              @delete="removeCriterion(index)"
-            >
-            </contact-criterion>
-          </oxd-grid>
-        </oxd-form-row>
-
-        <oxd-divider />
-        <oxd-form-row>
-          <oxd-text class="orangehrm-sub-title" tag="h6">
-            {{ $t('pim.display_fields') }}
-          </oxd-text>
           <oxd-grid :cols="4" class="orangehrm-full-width-grid">
             <oxd-grid-item>
-              <oxd-input-field
-                v-model="report.fieldGroup"
-                type="select"
-                :label="$t('pim.select_display_field_group')"
-                :options="availableFieldGroups"
+              <employee-autocomplete
+                v-model="filters.employeeNumber"
+                :rules="rules.employee"
+                api-path="/api/v2/directory/employees"
               />
             </oxd-grid-item>
-            <oxd-grid-item class="orangehrm-report-criteria --span-column-2">
+            <oxd-grid-item>
               <oxd-input-field
-                v-model="report.displayField"
+                v-model="filters.jobTitleId"
                 type="select"
-                :label="$t('pim.select_display_field')"
-                :options="availableDisplayFields"
+                :label="$t('general.job_title')"
+                :options="jobTitles"
               />
-              <oxd-input-group>
-                <oxd-icon-button
-                  class="orangehrm-report-icon"
-                  name="plus"
-                  @click="addDisplayField"
-                />
-              </oxd-input-group>
             </oxd-grid-item>
-            <contact-display-field
-              v-for="(fieldGroup, index) in report.fieldGroupSelected"
-              :key="fieldGroup"
-              v-model:includeHeader="
-                report.displayFieldSelected[fieldGroup.id].includeHeader
-              "
-              :field-group="fieldGroup"
-              :selected-fields="
-                report.displayFieldSelected[fieldGroup.id].fields
-              "
-              @delete="removeDisplayFieldGroup(index)"
-              @delete-chip="removeDisplayField($event, index)"
-            >
-            </contact-display-field>
+            <oxd-grid-item>
+              <oxd-input-field
+                v-model="filters.locationId"
+                type="select"
+                :label="$t('general.location')"
+                :options="locations"
+              />
+            </oxd-grid-item>
+            <oxd-grid-item>
+              <oxd-input-field
+                v-model="filters.keywords"
+                :label="$t('Keywords')"
+                :placeholder="$t('Search Keywords')"
+              />
+            </oxd-grid-item>
           </oxd-grid>
         </oxd-form-row>
 
         <oxd-divider />
+
         <oxd-form-actions>
           <oxd-button
-            type="button"
+            type="reset"
             display-type="ghost"
-            :label="$t('general.cancel')"
-            @click="onCancel"
+            :label="$t('general.reset')"
           />
-          <oxd-button
-            type="submit"
-            display-type="secondary"
-            :label="$t('general.save')"
-          />
-          <oxd-button
-            type="button"
-            display-type="secondary"
-            :label="'Export'"
-            @click="onExport"
-          />
+          <submit-button :label="$t('general.search')" />
         </oxd-form-actions>
       </oxd-form>
-    </div>
+    </oxd-table-filter>
 
-    <reports-table
-      v-if="showTable"
-      module="directory"
-      name="employee_contact_list"
-      :filters="filters"
-      :can-focus="true"
-    >
-      <div class="orangehrm-card-container">
+    <br />
+
+    <div class="orangehrm-paper-container">
+      <div class="orangehrm-header-container">
         <oxd-text tag="h6" class="orangehrm-main-title">
-          {{ report.name }}
+          {{ $t('Contact List Table') }}
         </oxd-text>
       </div>
-      <br />
-    </reports-table>
+      <table-header
+        :selected="0"
+        :total="total"
+        :loading="isLoading"
+      ></table-header>
+      <div ref="scrollerRef" class="orangehrm-container">
+        <oxd-card-table
+          :headers="headers"
+          :items="employees"
+          :selectable="false"
+          :clickable="false"
+          :loading="isLoading"
+          row-decorator="oxd-table-decorator-card"
+        />
+        <oxd-loading-spinner
+          v-if="isLoading"
+          class="orangehrm-container-loader"
+        />
+      </div>
+      <div class="orangehrm-bottom-container">
+        <oxd-pagination
+          v-if="showPaginator"
+          v-model:current="currentPage"
+          :length="pages"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import {ref, computed} from 'vue';
-import {navigate} from '@ohrm/core/util/helper/navigation';
+import {reactive, toRefs, computed} from 'vue';
+import useSort from '@ohrm/core/util/composable/useSort';
+import usePaginate from '@ohrm/core/util/composable/usePaginate';
+import {APIService} from '@/core/util/services/api.service';
+import EmployeeAutocomplete from '@/core/components/inputs/EmployeeAutocomplete';
+import useInfiniteScroll from '@ohrm/core/util/composable/useInfiniteScroll';
 import {
-  required,
   shouldNotExceedCharLength,
-} from '@ohrm/core/util/validation/rules';
-import {APIService} from '@ohrm/core/util/services/api.service';
-import ContactCriterion from '@/orangehrmCorporateDirectoryPlugin/components/ContactCriterion';
-import ContactDisplayField from '@/orangehrmCorporateDirectoryPlugin/components/ContactDisplayField';
-import useEmployeeReport from '@/orangehrmCorporateDirectoryPlugin/util/composable/useEmployeeReport';
-import ReportsTable from '@/core/components/table/ReportsTable';
+  validSelection,
+} from '@/core/util/validation/rules';
+import {OxdSpinner} from '@ohrm/oxd';
+import usei18n from '@/core/util/composable/usei18n';
+import useToast from '@/core/util/composable/useToast';
+
+const defaultFilters = {
+  employeeNumber: null,
+  jobTitleId: null,
+  locationId: null,
+  keywords: '',
+};
 
 export default {
   name: 'EmployeeContactList',
+
   components: {
-    'contact-criterion': ContactCriterion,
-    'contact-display-field': ContactDisplayField,
-    'reports-table': ReportsTable,
+    'employee-autocomplete': EmployeeAutocomplete,
+    'oxd-loading-spinner': OxdSpinner,
   },
 
   props: {
-    selectionCriteria: {
+    jobTitles: {
       type: Array,
-      required: true,
+      default: () => [],
     },
-    displayFieldGroups: {
+    locations: {
       type: Array,
-      required: true,
-    },
-    displayFields: {
-      type: Array,
-      required: true,
+      default: () => [],
     },
   },
 
-  setup(props) {
+  setup() {
+    const {$t} = usei18n();
+    const {noRecordsFound} = useToast();
+
+    const rules = {
+      employee: [shouldNotExceedCharLength(100), validSelection],
+    };
+
+    const employeeDataNormalizer = (data) => {
+      return data.map((item) => {
+        return {
+          id: item.empNumber,
+          employeeName:
+            `${item.firstName} ${item.middleName} ${item.lastName} ` +
+            (item.terminationId ? $t('general.past_employee') : ''),
+          employeeJobTitle: item.jobTitle?.isDeleted
+            ? `${item.jobTitle?.title} ` + $t('general.deleted')
+            : item.jobTitle?.title,
+          employeeSubUnit: item.subunit?.name,
+          employeeLocation: item.location?.name,
+        };
+      });
+    };
+
     const http = new APIService(
       window.appGlobal.baseUrl,
-      '/api/v2/directory/contact-list',
+      '/api/v2/directory/contacts',
     );
 
-    const showTable = ref(false);
+    const state = reactive({
+      total: 0,
+      employees: [],
+      isLoading: false,
+      filters: {
+        ...defaultFilters,
+      },
+    });
+
+    const {sortDefinition, onSort} = useSort({
+      sortDefinition: {
+        'employee.firstName': 'ASC',
+        'employee.lastName': 'DEFAULT',
+        'jobTitle.jobTitleName': 'DEFAULT',
+        'location.name': 'DEFAULT',
+      },
+    });
+
+    const serializedFilters = computed(() => {
+      return {
+        employeeNumber: state.filters.employeeNumber?.id,
+        jobTitleId: state.filters.jobTitleId?.id,
+        locationId: state.filters.locationId?.id,
+        keywords: state.filters.keywords,
+      };
+    });
 
     const {
-      report,
-      addCriterion,
-      serializeBody,
-      addDisplayField,
-      removeCriterion,
-      removeDisplayField,
-      removeDisplayFieldGroup,
-      availableCriteria,
-      availableFieldGroups,
-      availableDisplayFields,
-    } = useEmployeeReport(
-      props.selectionCriteria,
-      props.displayFields,
-      props.displayFieldGroups,
-    );
+      showPaginator,
+      currentPage,
+      total,
+      pages,
+      pageSize,
+      response,
+      isLoading,
+      execQuery,
+    } = usePaginate(http, {
+      query: serializedFilters,
+    });
 
-    const filters = computed(() => ({
-      reportId: report.value.id,
-    }));
+    onSort(execQuery);
+
+    const fetchData = () => {
+      state.isLoading = true;
+      execQuery()
+        .then((response) => {
+          const {data, meta} = response.data;
+          state.total = meta?.total || 0;
+          if (Array.isArray(data)) {
+            state.employees = [
+              ...state.employees,
+              ...employeeDataNormalizer(data),
+            ];
+          }
+          if (state.total === 0) {
+            noRecordsFound();
+          }
+        })
+        .finally(() => (state.isLoading = false));
+    };
+
+    const {scrollerRef} = useInfiniteScroll(fetchData);
 
     return {
       http,
-      report,
-      showTable,
-      filters,
-      addCriterion,
-      serializeBody,
-      addDisplayField,
-      removeCriterion,
-      removeDisplayField,
-      removeDisplayFieldGroup,
-      availableCriteria,
-      availableFieldGroups,
-      availableDisplayFields,
+      rules,
+      ...toRefs(state),
+      sortDefinition,
+      currentPage,
+      total,
+      pages,
+      pageSize,
+      showPaginator,
+      scrollerRef,
+      fetchData,
+      onSort,
+      execQuery,
     };
   },
 
   data() {
     return {
-      isLoading: false,
-      rules: {
-        name: [required, shouldNotExceedCharLength(250)],
-        includeEmployees: [required],
-      },
-      includeOpts: [
+      headers: [
         {
-          id: 1,
-          label: this.$t('general.current_employees_only'),
+          name: 'fullName',
+          slot: 'title',
+          title: this.$t('general.name'),
+          style: {flex: 2},
         },
         {
-          id: 2,
-          label: this.$t('general.current_and_past_employees'),
+          name: 'jobTitle',
+          title: this.$t('general.job_title'),
+          style: {flex: 2},
         },
         {
-          id: 3,
-          label: this.$t('general.past_employees_only'),
+          name: 'location',
+          title: this.$t('general.location'),
+          style: {flex: 2},
+        },
+        {
+          name: 'telephone',
+          title: this.$t('Telephone'),
+          style: {flex: 2},
+        },
+        {
+          name: 'email',
+          title: this.$t('general.email'),
+          style: {flex: 2},
         },
       ],
     };
   },
 
+  beforeMount() {
+    this.fetchData();
+  },
+
   methods: {
-    onCancel() {
-      navigate('/directory/viewDirectory');
+    onSearch() {
+      this.fetchData();
     },
-    onSave() {
-      this.isLoading = true;
-      const payload = this.serializeBody(this.report);
-      this.http
-        .create(payload)
-        .then((response) => {
-          const {data} = response.data;
-          this.report.id = data.id;
-          return this.$toast.saveSuccess();
-        })
-        .then(() => {
-          this.showTable = true;
-        })
-        .finally(() => {
-          this.isLoading = false;
-        });
-    },
-    onExport() {
-      this.isLoading = true;
-      const payload = this.serializeBody(this.report);
-      this.http
-        .request({
-          method: 'POST',
-          url: 'export',
-          data: payload,
-          responseType: 'blob',
-        })
-        .then((response) => {
-          const url = window.URL.createObjectURL(new Blob([response.data]));
-          const link = document.createElement('a');
-          link.href = url;
-          link.setAttribute('download', `${this.report.name}.csv`);
-          document.body.appendChild(link);
-          link.click();
-        })
-        .catch((error) => {
-          this.$toast.error({
-            title: this.$t('general.error'),
-            message: error.message,
-          });
-        })
-        .finally(() => {
-          this.isLoading = false;
-        });
+    onReset() {
+      this.filters = {...defaultFilters};
+      this.fetchData();
     },
   },
 };

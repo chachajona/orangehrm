086b67668446c294641ead7ffbf8264d85ea5cd7
Dung Dang <dung.dang@vietunion.com.vn>
2024-10-16 03:28:41 +0700

    add Export button (Contact List frontend)

--- src/client/src/orangehrmCorporateDirectoryPlugin/pages/contact-list/EmployeeContactList.vue
+++ src/client/src/orangehrmCorporateDirectoryPlugin/pages/contact-list/EmployeeContactList.vue
@@ -19,10 +19,10 @@
 
 <template>
   <div class="orangehrm-background-container">
-    <oxd-table-filter :filter-title="$t('Employee Contact List')">
+    <oxd-table-filter :filter-title="$t('Contact List')">
       <oxd-form @submit-valid="onSearch" @reset="onReset">
         <oxd-form-row>
-          <oxd-grid :cols="4" class="orangehrm-full-width-grid">
+          <oxd-grid :cols="4">
             <oxd-grid-item>
               <employee-autocomplete
                 v-model="filters.employeeNumber"
@@ -46,13 +46,12 @@
                 :options="locations"
               />
             </oxd-grid-item>
-            <oxd-grid-item>
-              <oxd-input-field
-                v-model="filters.keywords"
-                :label="$t('Keywords')"
-                :placeholder="$t('Search Keywords')"
-              />
-            </oxd-grid-item>
+            <oxd-input-field
+              v-model="filters.subUnitId"
+              type="select"
+              :label="$t('general.sub_unit')"
+              :options="subUnits"
+            />
           </oxd-grid>
         </oxd-form-row>
 
@@ -60,9 +59,15 @@
 
         <oxd-form-actions>
           <oxd-button
-            type="reset"
-            display-type="ghost"
             :label="$t('general.reset')"
+            display-type="ghost"
+            type="reset"
+          />
+          <oxd-button
+            type="button"
+            display-type="secondary"
+            :label="$t('Export')"
+            @click="onClickExport"
           />
           <submit-button :label="$t('general.search')" />
         </oxd-form-actions>
@@ -71,62 +76,63 @@
 
     <br />
 
-    <div class="orangehrm-paper-container">
-      <div class="orangehrm-header-container">
-        <oxd-text tag="h6" class="orangehrm-main-title">
-          {{ $t('Contact List Table') }}
-        </oxd-text>
-      </div>
-      <table-header
-        :selected="0"
-        :total="total"
-        :loading="isLoading"
-      ></table-header>
-      <div ref="scrollerRef" class="orangehrm-container">
-        <oxd-card-table
-          :headers="headers"
+    <div class="orangehrm-corporate-directory">
+      <slot :generate-report="generateContactList"></slot>
+      <div v-if="headers.length !== 0" class="orangehrm-paper-container">
+        <table-header
+          :selected="0"
+          :total="total"
+          :loading="false"
+          :show-divider="false"
+        ></table-header>
+        <oxd-report-table
           :items="employees"
-          :selectable="false"
-          :clickable="false"
+          :headers="headers"
           :loading="isLoading"
-          row-decorator="oxd-table-decorator-card"
-        />
-        <oxd-loading-spinner
-          v-if="isLoading"
-          class="orangehrm-container-loader"
-        />
-      </div>
-      <div class="orangehrm-bottom-container">
-        <oxd-pagination
-          v-if="showPaginator"
-          v-model:current="currentPage"
-          :length="pages"
-        />
+          :can-focus="canFocus"
+          :range="range"
+        >
+          <template #cell="{prop, row}">
+            {{ row[prop] }}
+          </template>
+          <template #header="{header}">
+            <template v-if="header.children">
+              <div>{{ header.name }}</div>
+              <div>
+                <span v-for="child in header.children" :key="child.prop">
+                  {{ child.name }}
+                </span>
+              </div>
+            </template>
+            <template v-else>
+              {{ header.name }}
+            </template>
+          </template>
+        </oxd-report-table>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import {reactive, toRefs, computed} from 'vue';
-import useSort from '@ohrm/core/util/composable/useSort';
-import usePaginate from '@ohrm/core/util/composable/usePaginate';
+import {ref, computed, onMounted, watch, onBeforeMount} from 'vue';
+import useToast from '@/core/util/composable/useToast';
 import {APIService} from '@/core/util/services/api.service';
-import EmployeeAutocomplete from '@/core/components/inputs/EmployeeAutocomplete';
-import useInfiniteScroll from '@ohrm/core/util/composable/useInfiniteScroll';
 import {
   shouldNotExceedCharLength,
   validSelection,
 } from '@/core/util/validation/rules';
-import {OxdSpinner} from '@ohrm/oxd';
-import usei18n from '@/core/util/composable/usei18n';
-import useToast from '@/core/util/composable/useToast';
+import {navigate} from '@ohrm/core/util/helper/navigation';
+import usePaginate from '@ohrm/core/util/composable/usePaginate';
+import EmployeeAutocomplete from '@/core/components/inputs/EmployeeAutocomplete';
+import {CellAdapter, OxdMultilineCell, OxdReportTable} from '@ohrm/oxd';
+import * as XLSX from 'xlsx';
 
 const defaultFilters = {
   employeeNumber: null,
   jobTitleId: null,
   locationId: null,
-  keywords: '',
+  subUnitId: null,
 };
 
 export default {
@@ -134,7 +140,7 @@ export default {
 
   components: {
     'employee-autocomplete': EmployeeAutocomplete,
-    'oxd-loading-spinner': OxdSpinner,
+    'oxd-report-table': OxdReportTable,
   },
 
   props: {
@@ -146,162 +152,193 @@ export default {
       type: Array,
       default: () => [],
     },
+    subUnits: {
+      type: Array,
+      default: () => [],
+    },
   },
 
   setup() {
-    const {$t} = usei18n();
     const {noRecordsFound} = useToast();
+    const total = ref(0);
+    const offset = ref(0);
+    const employees = ref([]);
+    const currentIndex = ref(-1);
+    const isLoading = ref(false);
+    const filters = ref({...defaultFilters});
+    const headers = ref([
+      {
+        name: 'Personal',
+        children: [
+          {name: 'Employee Id', prop: 'employeeId', size: 100},
+          {name: 'Last Name', prop: 'lastName', size: 200},
+          {name: 'First Name', prop: 'firstName', size: 200},
+          {name: 'Middle Name', prop: 'middleName', size: 200},
+        ],
+      },
+      {
+        name: 'Job',
+        children: [
+          {name: 'Job Title', prop: 'jobTitle', size: 200},
+          {name: 'Sub Unit', prop: 'subUnit', size: 200},
+        ],
+      },
+    ]);
+    const canFocus = ref(true);
+    const range = ref(true);
+
+    const http = new APIService(
+      window.appGlobal.baseUrl,
+      '/api/v2/directory/employees',
+    );
 
     const rules = {
       employee: [shouldNotExceedCharLength(100), validSelection],
     };
 
+    const serializedFilters = computed(() => {
+      return {...filters.value, name: 'employee_contact_list'};
+    });
+
     const employeeDataNormalizer = (data) => {
-      return data.map((item) => {
-        return {
-          id: item.empNumber,
-          employeeName:
-            `${item.firstName} ${item.middleName} ${item.lastName} ` +
-            (item.terminationId ? $t('general.past_employee') : ''),
-          employeeJobTitle: item.jobTitle?.isDeleted
-            ? `${item.jobTitle?.title} ` + $t('general.deleted')
-            : item.jobTitle?.title,
-          employeeSubUnit: item.subunit?.name,
-          employeeLocation: item.location?.name,
-        };
-      });
+      return data.map((item) => ({
+        id: item.empNumber,
+        employeeId: item.empNumber,
+        lastName: item.lastName,
+        firstName: item.firstName,
+        middleName: item.middleName,
+        jobTitle: item.jobTitle?.title || '--',
+        subUnit: item.subunit?.name || '--',
+        _rows: Math.max(
+          ...Object.values(item)
+            .filter(Array.isArray)
+            .map((arr) => arr.length),
+          1,
+        ),
+      }));
     };
 
-    const http = new APIService(
-      window.appGlobal.baseUrl,
-      '/api/v2/directory/contacts',
-    );
-
-    const state = reactive({
-      total: 0,
-      employees: [],
-      isLoading: false,
-      filters: {
-        ...defaultFilters,
-      },
-    });
-
-    const {sortDefinition, onSort} = useSort({
-      sortDefinition: {
-        'employee.firstName': 'ASC',
-        'employee.lastName': 'DEFAULT',
-        'jobTitle.jobTitleName': 'DEFAULT',
-        'location.name': 'DEFAULT',
-      },
-    });
-
-    const serializedFilters = computed(() => {
-      return {
-        employeeNumber: state.filters.employeeNumber?.id,
-        jobTitleId: state.filters.jobTitleId?.id,
-        locationId: state.filters.locationId?.id,
-        keywords: state.filters.keywords,
-      };
-    });
-
-    const {
-      showPaginator,
-      currentPage,
-      total,
-      pages,
-      pageSize,
-      response,
-      isLoading,
-      execQuery,
-    } = usePaginate(http, {
+    const {execQuery: fetchTableData} = usePaginate(http, {
       query: serializedFilters,
+      prefetch: false,
     });
-
-    onSort(execQuery);
 
     const fetchData = () => {
-      state.isLoading = true;
-      execQuery()
+      isLoading.value = true;
+      http
+        .getAll({
+          limit: 50,
+          offset: offset.value,
+          locationId: filters.value.locationId?.id,
+          empNumber: filters.value.employeeNumber?.id,
+          jobTitleId: filters.value.jobTitleId?.id,
+          subUnitId: filters.value.subUnitId?.id,
+        })
         .then((response) => {
           const {data, meta} = response.data;
-          state.total = meta?.total || 0;
+          total.value = meta?.total || 0;
           if (Array.isArray(data)) {
-            state.employees = [
-              ...state.employees,
-              ...employeeDataNormalizer(data),
-            ];
+            employees.value = employeeDataNormalizer(data);
           }
-          if (state.total === 0) {
+          if (total.value === 0) {
             noRecordsFound();
           }
         })
-        .finally(() => (state.isLoading = false));
+        .catch((error) => {
+          console.error('Error fetching employee data:', error);
+        })
+        .finally(() => (isLoading.value = false));
     };
 
-    const {scrollerRef} = useInfiniteScroll(fetchData);
+    const setupTableHeaders = () => {
+      headers.value = headers.value.map((header) => {
+        if (header.children) {
+          header.children = header.children.map((child) => ({
+            ...child,
+            cellProperties: ({prop, model}) => ({
+              onClick: model?._url
+                ? () => navigate(model._url[prop])
+                : undefined,
+            }),
+            cellTemplate: CellAdapter(OxdMultilineCell),
+          }));
+        }
+        return header;
+      });
+    };
+
+    const setupTableData = () => {
+      employees.value = employees.value.map((item) => {
+        let _rows = 0;
+        for (const key in item) {
+          const value = item[key];
+          if (Array.isArray(value) && value.length > _rows)
+            _rows = value.length;
+        }
+        return {...item, _rows};
+      });
+    };
+
+    const onSearch = () => {
+      employees.value = [];
+      offset.value = 0;
+      fetchData();
+    };
+
+    const onReset = () => {
+      employees.value = [];
+      offset.value = 0;
+      filters.value = {...defaultFilters};
+      fetchData();
+    };
+
+    const onClickExport = () => {
+      const data = employees.value.map((employee) => ({
+        'Employee Id': employee.employeeId,
+        'Last Name': employee.lastName,
+        'First Name': employee.firstName,
+        'Middle Name': employee.middleName,
+        'Job Title': employee.jobTitle,
+        'Sub Unit': employee.subUnit,
+      }));
+
+      const worksheet = XLSX.utils.json_to_sheet(data);
+      const workbook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(workbook, worksheet, 'Employees');
+      XLSX.writeFile(workbook, 'employee_contact_list.xlsx');
+    };
+
+    const generateContactList = async () => {
+      if (headers.value.length === 0) await setupTableHeaders();
+      await setupTableData();
+    };
+
+    watch(filters, () => {
+      fetchData();
+    });
+
+    onBeforeMount(() => {
+      generateContactList();
+      fetchData();
+    });
 
     return {
-      http,
-      rules,
-      ...toRefs(state),
-      sortDefinition,
-      currentPage,
       total,
-      pages,
-      pageSize,
-      showPaginator,
-      scrollerRef,
-      fetchData,
-      onSort,
-      execQuery,
+      offset,
+      employees,
+      currentIndex,
+      isLoading,
+      filters,
+      headers,
+      canFocus,
+      range,
+      rules,
+      serializedFilters,
+      fetchTableData,
+      onSearch,
+      onReset,
+      onClickExport,
     };
-  },
-
-  data() {
-    return {
-      headers: [
-        {
-          name: 'fullName',
-          slot: 'title',
-          title: this.$t('general.name'),
-          style: {flex: 2},
-        },
-        {
-          name: 'jobTitle',
-          title: this.$t('general.job_title'),
-          style: {flex: 2},
-        },
-        {
-          name: 'location',
-          title: this.$t('general.location'),
-          style: {flex: 2},
-        },
-        {
-          name: 'telephone',
-          title: this.$t('Telephone'),
-          style: {flex: 2},
-        },
-        {
-          name: 'email',
-          title: this.$t('general.email'),
-          style: {flex: 2},
-        },
-      ],
-    };
-  },
-
-  beforeMount() {
-    this.fetchData();
-  },
-
-  methods: {
-    onSearch() {
-      this.fetchData();
-    },
-    onReset() {
-      this.filters = {...defaultFilters};
-      this.fetchData();
-    },
   },
 };
 </script>
